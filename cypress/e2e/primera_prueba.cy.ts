d2f30c2772fc149d09d7ab3d0b7253038fcbee05
Esteban Ovalle Aguilar <eovalle@arkusnexus.com>
2025-03-20 01:01:14 -0600

    Prueba agregar, editar y eliminar agregados

--- cypress/e2e/primera_prueba.cy.ts
+++ cypress/e2e/primera_prueba.cy.ts
@@ -1,6 +1,35 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
-    cy.get('h1').should('contain','XLista de Actovodades')
+import {e2e} from '../support/constantes'
+
+const nombreDeActividad = 'Nueva actividad';
+const nuevoNombreDeActividad = 'Nueva actividad';
+
+describe('List de actividades', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Agregar una actividad', () => {
+    cy.get(e2e.CAMPO_AGREGAR_ACTIVIDAD).type(nombreDeActividad);
+    cy.get(e2e.BOTON_AGREGAR).click();
+    cy.contains('ul.actividades-list li.actividad-item', nombreDeActividad).should('be.visible');
+  })
+
+  describe('Eliminar/Editar de actividades', () => {
+    beforeEach(() => {
+      cy.get(e2e.CAMPO_AGREGAR_ACTIVIDAD).type(nombreDeActividad);
+      cy.get(e2e.BOTON_AGREGAR).click();
+    });
+  
+    it('Editar una actividad', () => {
+      cy.get(e2e.BOTON_EDITAR).click();
+      cy.get(e2e.CAMPO_AGREGAR_ACTIVIDAD).type(nuevoNombreDeActividad);
+      cy.get(e2e.BOTON_GUARDAR).click();
+      cy.contains('ul.actividades-list li.actividad-item', nombreDeActividad).should('be.visible');
+    })
+  
+    it('Eliminar una actividad', () => {
+      cy.get(e2e.BOTON_ELIMINAR).click();
+      cy.contains('ul.actividades-list li.actividad-item', nombreDeActividad).should('not.exist');
+    })
   })
 })
---
+++ cypress/support/constantes.ts
@@ -0,0 +1,9 @@
+export const e2e = {
+    TITULO_PRINCIPAL: '',
+    SUBTITULO: '',
+    CAMPO_AGREGAR_ACTIVIDAD: '[placeholder="Nombre de la actividad"]',
+    BOTON_AGREGAR: '[class="btn btn-add"]',
+    BOTON_ELIMINAR: '[class="btn btn-delete"]',
+    BOTON_EDITAR: '[class="btn btn-edit"]',
+    BOTON_GUARDAR: '[class="btn btn-save"]'
+}
